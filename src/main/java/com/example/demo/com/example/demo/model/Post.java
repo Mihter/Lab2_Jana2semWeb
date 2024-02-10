3a163139d414acf39b3548cda43d1f72d4b36881
Kazmin Mihail <mihail.kazmin.04@mail.ru>
2024-02-11 00:07:43 +0500

    lab 2 bootStrap

--- src/main/java/com/example/demo/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/com/example/demo/model/Post.java
@@ -1,14 +1,18 @@
 package com.example.demo.com.example.demo.model;
 
 
+import java.util.Date;
+
 public class Post
 {
     private String text;
     private Integer likes;
+    private Date creationDate;
 
-    public Post(String text)
+    public Post(final String text, final Date creationDate)
     {
         this.text = text;
+        this.creationDate = creationDate;
     }
 
     public String getText()
@@ -19,5 +23,9 @@ public class Post
     public Integer getLikes()
     {
         return likes;
+    }
+    public Date getCreationDate()
+    {
+        return creationDate;
     }
 }
--- src/main/java/com/example/demo/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/com/example/demo/service/PostService.java
@@ -4,6 +4,8 @@ import com.example.demo.com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -11,14 +13,13 @@ public class PostService
 {
     public List<Post> listAllPosts()//возвращаю 3 поста из листа постов
     {
-        Post post1 = new Post("Сессия прошла!");
-        Post post2 = new Post("Не многие уцелели!");
-        Post post3 = new Post("Но никто не сдаётся!");
+        Post post1 = new Post("Сессия прошла!", new Date(124, Calendar.AUGUST, 3));//1900+124 = 2024 год)
+        Post post2 = new Post("Не многие уцелели!", new Date(124, Calendar.AUGUST, 4));
+        Post post3 = new Post("Но никто не сдаётся!", new Date(124, Calendar.AUGUST, 5));
         List<Post> posts = new ArrayList<Post>();
         posts.add(post1);
         posts.add(post2);
         posts.add(post3);
         return posts;
     }
-
 }
